1bbf1103cb2f187d9fe2b92602661ae9ea302087
Haruka <saito75061@gmail.com>
2019-05-17 17:33:39 +0900

    test code refactor

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
   test "should get home(URL[Home]のテスト。GETリクエストをhomeアクションに対して発行 (=送信) 。レスポンスはリクエストに対して[成功]を返す。タイトルはRuby on Rails Tutorial Sample App)" do
     get static_pages_home_url
     assert_response :success
-    assert_select 'title', "Home | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', "Home | #{@base_title}"
   end
 
   test "should get help(URL[Help]ページのテスト。GETリクエストをhelpアクションに対して発行 (=送信) 。レスポンスはリクエストに対して[成功]を返す。タイトルはRuby on Rails Tutorial Sample App)" do
     get static_pages_help_url
     assert_response :success
-    assert_select 'title', "Help | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', "Help | #{@base_title}"
   end
 
  test "should get about page(URL[About]ページのテスト。GETリクエストをaboutアクションに対して発行 (=送信) 。レスポンスはリクエストに対して[200-299]を返す。タイトルはRuby on Rails Tutorial Sample App)" do
    get static_pages_about_url
    assert_response :success
-   assert_select 'title', "About | Ruby on Rails Tutorial Sample App"
+   assert_select 'title', "About | #{@base_title}"
  end
 
 end
