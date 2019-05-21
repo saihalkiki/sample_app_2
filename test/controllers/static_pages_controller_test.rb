db6d3c74392c67d23f56eb0b43108d29ee855768
Haruka <saito75061@gmail.com>
2019-05-21 16:16:24 +0900

    modify routes test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,25 +7,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get root(rootルーティング(home)のテスト。レスポンスはリクエストに対して[成功]を返す。タイトルはRuby on Rails Tutorial Sample App)" do
-    get root_url
+    get root_path
     assert_response :success
     assert_select 'title', "#{@base_title}"
   end
 
   test "should get help(URL[Help]ページのテスト。GETリクエストをhelpアクションに対して発行 (=送信) 。レスポンスはリクエストに対して[成功]を返す。タイトルはRuby on Rails Tutorial Sample App)" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select 'title', "Help | #{@base_title}"
   end
 
  test "should get about page(URL[About]ページのテスト。GETリクエストをaboutアクションに対して発行 (=送信) 。レスポンスはリクエストに対して[200-299]を返す。タイトルはRuby on Rails Tutorial Sample App)" do
-   get static_pages_about_url
+   get about_path
    assert_response :success
    assert_select 'title', "About | #{@base_title}"
  end
 
  test "should get contact(URL[Contact]ページのテスト。GETリクエストをcontactアクションに対して発行 (=送信) 。レスポンスはリクエストに対して[200-299]を返す。タイトルはRuby on Rails Tutorial Sample App)" do
-   get static_pages_contact_url
+   get contact_path
    assert_response :success
    assert_select "title", "Contact | #{@base_title}"
  end
