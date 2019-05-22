c9fa8ad6cb7177c01299fc1fbf7f1d645afd2180
Haruka <saito75061@gmail.com>
2019-05-22 12:31:32 +0900

    add test:models valid

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User",email: "user@example")
+  end
+
+  test "should be vaild" do
+    assert @user.valid?, "バリデーションに引っかかってfalseを返してます"
+  end
+
 end
