3af5ffacd56ca600c82aeb2d94b13d1d01020bcf
Haruka <saito75061@gmail.com>
2019-05-23 12:18:52 +0900

    add validate_uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   validates(:name, presence: true, length: { maximum: 50 })
-  validates(:email, presence: true, length: { maximum: 255})
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates(:email, presence: true, length: { maximum: 255},
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: {case_sensitive: false})
+            # validates(format: { with: OOOOOOOOOO }) → メールアドレスのフォーマットを検証
+            # validates(uniqueness: ture) → 一意性の検証オプション
+            # case_sensitive: → true:大文字小文字を区別する false:大文字小文字を区別しない
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User",email: "user@example")
+    @user = User.new(name: "Example User", email: "user@example.com")
   end
 
   test "should be vaild" do
@@ -21,12 +21,28 @@ class UserTest < ActiveSupport::TestCase
 
  test "name should not be too long(名前は長すぎてはいけません)" do
    @user.name = "a" * 51
-   assert_not @user.valid?
+   assert_not @user.valid?, "名前が51文字以上登録されております"
  end
 
  test "email should not be too long" do
    @user.email = "a" * 244 + "@example.com"
-   assert_not @user.valid?
+   assert_not @user.valid?, "メールが256文字以上入ります"
  end
+
+ test "email validation should reject invalid addresses" do
+   invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                          foo@bar_baz.com foo@bar+baz.com]
+   invalid_addresses.each do |invalid_address|
+     @user.email = invalid_address
+     assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+   end
+ end
+
+  test "email addresses should be unique(大文字小文字を区別しない一意性のテスト)" do
+    duplicate_user = @user.dup        # userを複製(duplicate_user)→複製しないとid情報がかぶるため、上書き扱いになる
+    duplicate_user.email =@user.email.upcase  # 大小文字の検証のため、duplicate_userのアドレスを大文字にする
+    @user.save                        # userを保存
+    assert_not duplicate_user.valid?  # userとduplicate_userが一緒であれば無効かどうか検証
+  end
 
 end
