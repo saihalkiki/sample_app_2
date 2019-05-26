4f9786263dcd65d45cc126a6797c0db266e2075c
Haruka <saito75061@gmail.com>
2019-05-26 21:28:52 +0900

    added user_sighup_error_message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,21 @@ class UsersController < ApplicationController
     @user = User.new
     # debugger
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'users/new'
+    end
+  end
+
+  # Strong Parameters
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+
 end
