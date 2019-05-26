a6c9b34d38684ef7503833899afdd4bccb42e733
Haruka <saito75061@gmail.com>
2019-05-26 15:32:20 +0900

    added users_show_pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,6 @@ class UsersController < ApplicationController
   end
 
   def new
+    # debugger
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
+  # 名前付きルートが使用可能に
+  # '/help' → help_path
+  # 'http:www.expmple.com/help' → help_url
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
@@ -9,16 +12,22 @@ Rails.application.routes.draw do
   resources :users
   # GET	    /users	      index	  users_path
   # すべてのユーザーを一覧するページ
+
   # GET	    /users/1	    show	  user_path(user)
   # 特定のユーザーを表示するページ
+
   # GET	    /users/new	  new	    new_user_path
   # ユーザーを新規作成するページ (ユーザー登録)
+
   # POST	  /users	      create	users_path
   # ユーザーを作成するアクション
+
   # GET	    /users/1/edit	edit	  edit_user_path(user)
   # id=1のユーザーを編集するページ
+
   # PATCH	  /users/1	    update	user_path(user)
   # ユーザーを更新するアクション
+
   # DELETE	/users/1	    destroy	user_path(user)
   # ユーザーを削除するアクション
 
