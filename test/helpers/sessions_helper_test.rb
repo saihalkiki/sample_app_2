3fe307e2ca3b9b1e6faeacd46a2127d0a36260cf
Haruka <saito75061@gmail.com>
2019-06-02 14:48:54 +0900

    modify session_helper_test comment

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -11,11 +11,12 @@ class SessionsHelperTest < ActionView::TestCase
 
     test"current_user returns right user when sessions is nil" do
       assert_equal @user, current_user # current_userとmichaelが同じかどうかテスト
+      # assert_equalの引数は「期待する値, 実際の値」の順序で書く点に注意
       assert is_logged_in? # session[:user_id]が空ならfalse、空じゃない（ログインしていれば)true
     end
 
     test "current_user returns nil when remember digest is wrong" do
       @user.update_attribute(:remember_digest, User.digest(User.new_token))   # @userのremember_digestが、ハッシュ化したremember_tokenをdigest化した値と同じなら、remember_digestを更新する
-      assert_nil current_user # 現在のユーザーがnilならtrue(@userが更新できない場合、現在のユーザーがnilになるかどうか検証)
+      assert_nil current_user # current_userがnilならtrue(@userが更新できない場合、current_userがnilになるかどうか検証)
     end
 end
