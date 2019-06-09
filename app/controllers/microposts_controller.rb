222a62e56f19566b44f0f048d648a405d43612e1
Haruka <saito75061@gmail.com>
2019-06-09 14:45:32 +0900

    Relationship model create

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -15,17 +15,17 @@ class MicropostsController < ApplicationController
   end
 
 # マイクロポストの削除アクション
-def destroy
-  @micropost.destroy
-  flash[:success] = "Micropost deleted"
-  redirect_to request.referrer || root_url
-  # request.referrerというメソッドを使っている。フレンドリーフォワーディングのrequest.url変数 (10.2.3) と似ていて、一つ前のURLを返します (今回の場合、Homeページになります)マイクロポストがHomeページから削除された場合でもプロフィールページから削除された場合でも、request.referrerを使うことでDELETEリクエストが発行されたページに戻すことができるので、非常に便利。元に戻すURLが見つからなかった場合でも (例えばテストではnilが返ってくることもあります)、リスト 13.52の||演算子でroot_urlをデフォルトに設定しているため、大丈夫。
-end
+  def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
+    # request.referrerというメソッドを使っている。フレンドリーフォワーディングのrequest.url変数 (10.2.3) と似ていて、一つ前のURLを返します (今回の場合、Homeページになります)マイクロポストがHomeページから削除された場合でもプロフィールページから削除された場合でも、request.referrerを使うことでDELETEリクエストが発行されたページに戻すことができるので、非常に便利。元に戻すURLが見つからなかった場合でも (例えばテストではnilが返ってくることもあります)、リスト 13.52の||演算子でroot_urlをデフォルトに設定しているため、大丈夫。
+  end
 
   private
 
-    def micropost_params
-      params.require(:micropost).permit(:content, :picture)
+  def micropost_params
+    params.require(:micropost).permit(:content)
       # :picture=>CarrierWave(画像アップローダー)をWebから更新できる許可リストにpicture属性を追加しましょう
     end
 
